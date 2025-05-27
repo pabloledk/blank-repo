20d32fe48238e491b80f905140eb9ee17998ae0d
Pablo Ledesma <pabloledk@gmail.com>
2025-05-27 18:37:14 -0400

    added grocery tracker

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,9 +1,16 @@
 let length;
 let width;
 function calculateArea(){
-    //isFloat() es parseFloat()
-    length = isFloat(document.getElementById("length").value);
-    width = isFloat(document.getElementById("width").value);
+    length = parseFloat(document.getElementById("length").value);
+    width = parseFloat(document.getElementById("width").value);
     let area = length * width;
     document.getElementById("result").innerText = `The area of the rentagle is ${area}.`;
+}
+
+function groceryTracker(){
+    let grocery1 = parseFloat(document.getElementById("grocery1").value);
+    let grocery2 = parseFloat(document.getElementById("grocery2").value);
+    let grocery3 = parseFloat(document.getElementById("grocery3").value);
+    let total = grocery1 + grocery2 + grocery3;
+    document.getElementById("result2").innerText = `The total amount is \$ ${total}.`;
 }
