f181d86b998e8296457a236bec48d89c09d5d4b1
Pablo Ledesma <pabloledk@gmail.com>
2025-05-28 15:17:34 -0400

    added followers count

---
+++ followersCount/followers_count.js
@@ -0,0 +1,18 @@
+let count = 0;
+function increaseCount(){
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+function displayCount(){
+    document.getElementById("countDisplay").innerText = count;
+}
+
+function checkCountValue(){
+if (count === 5){
+    alert("Your profile has gained 5 followers! Congrats");
+} else if (count===10){
+    alert("Your profile has gained 10 followers! Keep it up");
+}
+}
