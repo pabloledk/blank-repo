ad34a761d84e83767e48945e344bf090f4b40916
PabloLedesma <pabloledk@gmail.com>
2025-05-29 20:45:14 -0400

    new atribute added for employees

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,14 +1,14 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization:'Javascript' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization:'Python' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization:'Javascript' },
     //... More employee records can be added here
   ];
 
 
    // Function to display all employees
 function displayEmployees(){
-    const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+    const totalEmployees = employees.map((employee) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
     document.getElementById('employeesDetails').innerHTML = totalEmployees;
 }
 
@@ -32,3 +32,8 @@ function findEmployeeById(employeeId) {
       document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
      }
  }
+ function displaySpecializationJS(){
+    const JSEmployee = employees.filter(employee => employee.specialization === 'Javascript');
+    const JSEmployeeDisplay = JSEmployee.map((employee,index)=> `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+    document.getElementById('employeesDetails').innerHTML = JSEmployeeDisplay;
+}
