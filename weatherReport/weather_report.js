7826aa1271175d96fd3c5eb6ba6c0b62e785f187
Pablo Ledesma <pabloledk@gmail.com>
2025-06-11 19:21:47 -0400

    new lat/lon search

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -16,6 +16,21 @@ document.getElementById('weatherForm').addEventListener('submit',showweatherDeta
 // por qué esto tiene que hacerse asincronico y no sincrónico
 // por que van dos then, podría ir solo 1?
 
-
+function showweatherDetails2(event2) {
+    event2.preventDefault();
+    const lat = document.getElementById('lat').value;
+    const lon = document.getElementById('lon').value;
+    const apiKey = '8a244ab44f29188d4f217a4fd775a67c'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl2 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    fetch(apiUrl2)
+    .then(response => response.json())
+    .then(data2 => {
+      const weatherInfo2 = document.getElementById('weatherInfo2');
+      weatherInfo2.innerHTML = `<h2>Weather at lattitude ${data2.coord.lat} and longittude ${data2.coord.lon}</h2>
+                              <p>Temperature: ${data2.main.temp} &#8451;</p>
+                              <p>Weather: ${data2.weather[0].description}</p>`;
+    })
+}
+document.getElementById('weatherForm2').addEventListener('submit',showweatherDetails2 );
 // 8a244ab44f29188d4f217a4fd775a67c
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
